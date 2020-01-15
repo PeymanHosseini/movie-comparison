097397546863a8864867eb4e0ff420a71085a3db
Peyman Hosseini <peyman.66h@gmail.com>
2020-01-15 14:04:33 +0300

    requesting and getting data from API using axios

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "a596eaf0",
+      i: "tt00848228"
+    }
+  });
+  console.log(response.data);
+};
+fetchData();
