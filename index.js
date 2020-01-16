0084077ebedfe788e9a26fd1dcaecee056d20848
Peyman Hosseini <peyman.66h@gmail.com>
2020-01-16 08:00:42 +0300

    search data on input change

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const fetchData = async () => {
+// get data from API using axios lib.
+const fetchData = async search => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "a596eaf0",
-      i: "tt00848228"
+      s: search
     }
   });
   console.log(response.data);
 };
-fetchData();
+// get input from user and send it to API
+const input = document.querySelector("input");
+input.addEventListener("input", e => {
+  fetchData(e.target.value);
+});
