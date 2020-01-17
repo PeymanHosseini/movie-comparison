f37e21e86d198caac89a3d91edc30e7b5289d60b
Peyman Hosseini <peyman.66h@gmail.com>
2020-01-17 08:02:27 +0300

    implementing reusable debounse function

--- index.js
+++ index.js
@@ -8,15 +8,23 @@ const fetchData = async search => {
   });
   console.log(response.data);
 };
-// get input from user and send it to API
 const input = document.querySelector("input");
-let timeoutId;
-const onInput = e => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    fetchData(e.target.value);
-  }, 1000);
+// reusable function for delay
+const debounse = (func, delay = 1000) => {
+  let timeoutId;
+  // we use ... to spread arg or args to array and use 'apply' function to use it (if we would have arg)
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
 };
-input.addEventListener("input", onInput);
+
+// onInput function help us to delay search input for 1 sec
+const onInput = e => {
+  fetchData(e.target.value);
+};
+input.addEventListener("input", debounse(onInput, 500));
