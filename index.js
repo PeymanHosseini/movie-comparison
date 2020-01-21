f0c261d70c0d31bddac72b9433db0dcb95f4748c
Peyman Hosseini <peyman.66h@gmail.com>
2020-01-21 19:35:46 +0300

    autoComplete dropdown is done

--- index.js
+++ index.js
@@ -12,19 +12,35 @@ const fetchData = async search => {
 
   return response.data.Search;
 };
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label><b>Search for the movie</b></label>
+<input class="input" />
+<div class="dropdown">
+  <div class="dropdown-menu">
+      <div class="dropdown-content results">
+      </div>
+  </div>
+</div>
+`;
 const input = document.querySelector("input");
+const dropDown = document.querySelector(".dropdown");
+const resultWrapper = document.querySelector(".results");
 
 // onInput function help us to delay search input for 1 sec
 const onInput = async e => {
   const movies = await fetchData(e.target.value);
+
+  dropDown.classList.add("is-active");
   for (let movie of movies) {
-    const div = document.createElement("div");
-    div.innerHTML = `
+    const option = document.createElement("a");
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
     <img src="${movie.Poster}"></img>
-    <h1>${movie.Title}</h1>
+    ${movie.Title}
     `;
 
-    document.querySelector("#target").appendChild(div);
+    resultWrapper.appendChild(option);
   }
 };
 input.addEventListener("input", debounse(onInput, 500));
